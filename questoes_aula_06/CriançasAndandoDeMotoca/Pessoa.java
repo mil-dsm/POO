d01c674e319d0db6ebdca42c1d734e6506eaada5
Milena <milenadsmesquita@alu.ufc.br>
2025-10-05 17:53:06 -0300

    Atualização

--- questoes_aula_06/CriançasAndandoDeMotoca/Pessoa.java
+++ questoes_aula_06/CriançasAndandoDeMotoca/Pessoa.java
@@ -1,14 +1,22 @@
 package POO.questoes_aula_06.CriançasAndandoDeMotoca;
 
 public class Pessoa {
-    String name;
     int age;
+    String name;
+
+    public Pessoa(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
     public int getAge() {
         return age;
     }
+
     public String getName() {
         return name;
     }
+
     public String toString() {
         return name + ":" + age;
     }
